d6a879ac7cf900d38fd87225df29cbcd64e8524e
dzn1636 <tuandung.hoang0228@gmail.com>
2015-10-14 12:17:34 +0200

    première version de l'objet bonbon, décrit dans model.js

---
+++ model.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function bonbon(xx, yy) {
+    this.x = x;
+    this.y = yy;
+    this.dispo = 1;
+    this.changerDispo = function (newdispo) {
+        this.dispo = newdispo;
+    }
+}
+
+//function joueur(xx, yy, )  à voir une fois l'hypothèse est complétée
