d60b4af3966051c8050f809dcffe2c2cd2483c93
dzn1636 <tuandung.hoang0228@gmail.com>
2015-11-16 18:10:12 +0100

    améliorer les models

--- PlayerBonbon.js
+++ PlayerBonbon.js
@@ -6,23 +6,37 @@
 
 //non playable character
 
-var Bonbon = function(xx, yy) {
+var listeBonbons = new Array();
+var listeJoueurs = new Array();
+
+var Bonbon = function (xx, yy) {
     this.x = xx;
     this.y = yy;
-    this.disponible = 1;
     this.image = getImage("red_dot.png");
 };
 
-Bonbon.prototype.estMange = function() {
-    this.disponible = 0;
+Bonbon.prototype.estMange = function () { //le bonbon courant est mangé
+    for (index = 0; index < listeBonbons.length; index++) {
+        if ((listeBonbons[index].x === this.x) && (listeBonbons[index].y === this.y)) {
+            delete listeBonbons[index];
+        }
+    }
 };
 
-var b1 = new Bonbon(50,50);
-var b2 = new Bonbon(70,90);
+var nbAleaInf400 = function () {
+    Math.floor((Math.random() * 400) + 1);
+};
+
+var creerLesBonbons = function () {
+    for (index = 0; index < 10; index++) {
+        var bb = new Bonbon(new nbAleaInf400(), new nbAleaInf400());
+        listeBonbons.push(bb);
+    }
+};
 
 //playable character
 
-var Player = function(xx, yy) {
+var Joueur = function (xx, yy) {
     this.x = xx;
     this.y = yy;
     this.image = getImage("blue2.png");
@@ -30,38 +44,41 @@ var Player = function(xx, yy) {
 //    document.getElementsById("fenetreJeu").addEventListener("keyPressed", moveUp);
 };
 
-Player.prototype.draw = function() {
+Joueur.prototype.draw = function () {
     fill(255, 0, 0);
-    this.y = constraint(this.y, 0, height-50);
+    this.y = constraint(this.y, 0, height - 50);
     image(this.image, this.x, this.y, 40, 40);
 };
 
-Player.prototype.moveLeft = function() {
+Joueur.prototype.moveLeft = function () {
     this.image = getImage("blue3.png");
     this.x -= 10;
 };
 
-Player.prototype.moveRight = function() {
+Joueur.prototype.moveRight = function () {
     this.image = getImage("blue3.png");
     this.x += 10;
 };
 
-Player.prototype.moveUp = function() {
+Joueur.prototype.moveUp = function () {
     this.image = getImage("blue3.png");
     this.y -= 10;
 };
 
-Player.prototype.moveDown = function() {
+Joueur.prototype.moveDown = function () {
     this.image = getImage("blue3.png");
     this.x += 10;
 };
 
-var p1 = new Player(10,10);
+var j1 = new Joueur(10, 10);
+listeJoueurs.push(j1);
+var j2 = new Joueur(10, 50);
+listeJoueurs.push(j2);
 
-draw = function() {
-    background(255,255,255);
-    if(keyIsPressed && keyCode === 37) {
-        p1.moveLeft();
+draw = function () {
+    background(255, 255, 255);
+    if (keyIsPressed && keyCode === 37) {
+        j1.moveLeft();
     }
-    p1.draw();
+    j1.draw();
 };
