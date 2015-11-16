733b4b25b9b212e0e18da2a593a6146ff74aed23
VGanon <yannickjezequel@hotmail.fr>
2015-11-16 17:49:45 +0100

    Prise en main de socket.io

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 var express = require('express');
+var http = require ('http');
 var app = express();
-
-var http = require ('http').Server(app);
-var io = require('socket.io')(http);
+var server = module.exports = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 app.use(express.static('.'));
 app.get('/',function(req, res){
 	res.sendFile('./index.html' , { root : __dirname});
 });
 
-io.on('connection',function(socket){
-	console.log('a user is connected');
+io.sockets.on('connection',function(socket){	
+	socket.broadcast.emit('message', 'Un autre client vient de se connecter !');
+    // Quand le serveur reçoit un signal de type "message" du client    
+    socket.on('message', function (message) {
+		console.log(socket.pseudo + ' me parle ! Il me dit : ' + message);
+    }); 
+	
+	socket.on('petit_nouveau', function(pseudo) {
+		socket.pseudo = pseudo;
+    });
+
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+server.listen(8080, function(){
+  console.log('listening on *:8080');
 });
